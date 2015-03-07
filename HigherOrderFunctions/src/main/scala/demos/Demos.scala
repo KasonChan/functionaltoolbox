35cbc4deb5a229016fb0fa5be7aa4bf6e8fb9415
Kason <kasonl.chan@gmail.com>
2015-03-06 18:17:45 -0600

    Added list functions and higher order functions

--- HigherOrderFunctions/src/main/scala/demos/Demos.scala
+++ HigherOrderFunctions/src/main/scala/demos/Demos.scala
@@ -37,7 +37,7 @@ object Demos {
 
     /**
      * Map
-     * Takes a function and a sequence and applies that function to every 
+     * Takes a function and a sequence and applies that function to every
      * element in the sequence, producing a new sequence.
      */
     //    Add 3 to every element in the list.
@@ -62,11 +62,30 @@ object Demos {
     }
     echo(mapAdd3)
 
+    //    Converts every element to list
+    //    Set(List(b, l, u, e), List(g, r, e, e, n), List(o, r, a, n, g, e), List(r, e, d), List(p, u, r, p, l, e), List(y, e, l, l, o, w))
+    echo(colors map (_.toList))
+
     echo("")
 
     /**
+     * Flatmap
+     * Takes a function returning a list of elements as its right operand,
+     * applies the function to each list element and returns the concatenation
+     * of all function results.
+     */
+    //    Flatten every element and concatenate to list
+    //    Set(e, n, y, u, a, b, g, l, p, r, w, o, d)
+    echo(colors flatMap ((_).toList))
+
+    /**
+     * Foreach
+     * Takes a procedure as right operand, applies to each list element.
+     */
+
+    /**
      * Filter
-     * Takes a predicate and a sequence, and returns the sequence of elements 
+     * Takes a predicate and a sequence, and returns the sequence of elements
      * that satisfy that predicate.
      */
     //    Filter the numbers to get all the odd numbers
@@ -89,8 +108,39 @@ object Demos {
     echo("")
 
     /**
+     * Partition
+     * Takes a predicate and a sequence, and returns the sequence of elements
+     * that satisfy that predicate.
+     */
+    //    Partition the list into multiple of 3
+    //    (List(3, 6, 9),List(1, 2, 4, 5, 7, 8, 10))
+    echo(numbers partition (_ % 3 == 0))
+
+    //    Partition the red from the list
+    //    (Set(red),Set(purple, blue, orange, green, yellow))
+    echo(colors partition (_ == "red"))
+
+    echo("")
+
+    /**
+     * Find
+     * Takes a predicate and a sequence, and returns the first element
+     * satisfying a given predicate. Return Some(x) if predicate is true,
+     * otherwise return None.
+     */
+    //    Find multiple of two
+    //    Some(2)
+    echo(numbers find (_ % 2 == 0))
+
+    //    Find number that is smaller than zero
+    //    None
+    echo(numbers find (_ < 0))
+
+    echo("")
+
+    /**
      * Zip
-     * Takes two sequences and forms a list of pairs.
+     * Takes two or more sequences and forms a list of pairs.
      *
      * Unzip
      * Take any list of tuples and changes back to a tuple of lists.
@@ -152,8 +202,8 @@ object Demos {
 
     /**
      * Fold, fold left, fold right
-     * A fold takes a binary function, a starting value called accumulator and 
-     * a sequence to fold up. 
+     * A fold takes a binary function, a starting value called accumulator and
+     * a sequence to fold up.
      */
     //    Fold the numbers starting with accumulator 0
     //    15
@@ -178,6 +228,99 @@ object Demos {
     val sum5 = (numbers :\ 0)(_ + _)
     echo(sum5)
 
+    echo("")
+
+    /**
+     * List operations
+     */
+    //    Print the length
+    //    10
+    echo(numbers.length)
+
+    //    Print the rest of the list except the last
+    //    List(1, 2, 3, 4, 5, 6, 7, 8, 9)
+    echo(numbers.init)
+
+    //    Print the last element
+    //    10
+    echo(numbers.last)
+
+    //    Print the first element of the list
+    //    1
+    echo(numbers.head)
+
+    //    Print the rest of the list except the first element
+    //    List(2, 3, 4, 5, 6, 7, 8, 9, 10)
+    echo(numbers.tail)
+
+    //    Print the reverse of the list
+    //    List(10, 9, 8, 7, 6, 5, 4, 3, 2, 1)
+    echo(numbers.reverse)
+
+    //    Print the first two elements of the list
+    //    List(1, 2)
+    echo(numbers.take(2))
+
+    //  Print all the elements of the list except the first 7 ones
+    //    List(8, 9, 10)
+    echo(numbers.drop(7))
+
+    //    Print the pair of two lists splits at index 4
+    //    (List(1, 2, 3, 4),List(5, 6, 7, 8, 9, 10))
+    echo(numbers.splitAt(4))
+
+    //    Print the numbers at index
+    //    7
+    echo(numbers apply 6)
+
+    //    Print the indices of the elements
+    //    Range(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+    echo(numbers indices)
+
+    //    Print the set of numbers
+    //    List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    echo(List(numbers, numbers).flatten)
+    //    List()
+    echo(List(List(), List()).flatten)
+
+    //    Print the string of numbers
+    //    List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    echo(numbers.toString())
+    //    {1-2-3-4-5-6-7-8-9-10}
+    echo(numbers.mkString("{", "-", "}"))
+
+    //    Access list elements via an iterator
+    val it = numbers.iterator
+    //    true
+    echo(it.hasNext)
+    //    1
+    echo(it.next)
+
+    //    Sort the numbers
+    //    List(10, 9, 8, 7, 6, 5, 4, 3, 2, 1)
+    echo(numbers.sortWith(_ > _))
+
+    //    Create a range of numbers
+    //    List.range(from, until)
+    //    List(9, 7, 5, 3)
+    echo(List.range(9, 1, -2))
+    //    List(1, 3, 5, 7)
+    echo(List.range(1, 9, 2))
+
+    //    Creating uniform list
+    //    List(hello, hello, hello)
+    echo(List.fill(3)("hello"))
+    //     List(List(a, a, a), List(a, a, a))
+    echo(List.fill(2, 3)("a"))
+
+    //    Creates a list whose elements are computed according to a supplied
+    //    function
+    //    List(1, 4, 9, 16, 25)
+    echo((List.tabulate(6)(n => if (n != 0) n * n).tail))
+
+    //    Concatenates lists together
+    //    List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    echo(List.concat(numbers, numbers))
   }
 
   //  Add list to the parameter x such that List(x - 1, x, x + 1)
